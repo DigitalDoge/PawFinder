7e4b6f2e531dee0bb0f216df3043e7ee9223d9ae
DigitalDoge <DigitalDoggo69@gmail.com>
2024-11-02 12:18:50 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-    <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-BG5YPG7VDT"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-BG5YPG7VDT');
-</script>
-
-
-
 const labels = document.querySelectorAll('.form-control label')
 
 labels.forEach(label => {
